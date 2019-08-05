4f2631f22b2bc5ec5aac65f6adc12fdd4e93412b
Prashant rawal <Prashantrawal619@gmail.com>
2019-08-05 17:57:39 +0530

    complete lottery app

---
+++ lottery-app/src/Lottery.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Ball from "./Ball";
+import "./Lottery.css";
+export default class Lottery extends Component {
+  static defaultProps = {
+    title: "Lotto",
+    numBalls: 6,
+    maxNum: 40
+  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nums: Array.from({ length: this.props.numBalls })
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  generate() {
+    this.setState(currState => ({
+      nums: currState.nums.map(
+        n => Math.floor(Math.random() * this.props.maxNum) + 1
+      )
+    }));
+  }
+
+  handleClick() {
+    this.generate();
+  }
+
+  render() {
+    return (
+      <section className='Lottery'>
+        <h1>{this.props.title}</h1>
+        <div>
+          {this.state.nums.map(n => (
+            <Ball num={n} />
+          ))}
+        </div>
+        <button onClick={this.handleClick}>Generate</button>
+      </section>
+    );
+  }
+}
