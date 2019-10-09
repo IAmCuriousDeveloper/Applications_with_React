151fccae53f623d097b6cc700f50b5d1ca0b3c25
Prashant rawal <Prashantrawal619@gmail.com>
2019-10-09 17:15:31 +0530

    understand the rule.js file

--- yahtzee-app/src/Game.jsx
+++ yahtzee-app/src/Game.jsx
@@ -3,6 +3,7 @@ import Dice from "./Dice";
 import ScoreTable from "./ScoreTable";
 import "./Game.css";
 
+// no. of dice and rolls are fixed
 const NUM_DICE = 5;
 const NUM_ROLLS = 3;
 
@@ -10,9 +11,13 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      //empty array of length 5
       dice: Array.from({ length: NUM_DICE }),
+      // making new array with false filled in all
       locked: Array(NUM_DICE).fill(false),
+      // pre assign the rolls left to 3
       rollsLeft: NUM_ROLLS,
+
       scores: {
         ones: undefined,
         twos: undefined,
@@ -37,9 +42,11 @@ class Game extends Component {
   roll(evt) {
     // roll dice whose indexes are in reroll
     this.setState(st => ({
+      //take every dice from array dice then check if the value of that index in locked array is true/false then take decision
       dice: st.dice.map((d, i) =>
         st.locked[i] ? d : Math.ceil(Math.random() * 6)
       ),
+      // if we have rolls left then just pass the previous loacked otherwise fill locked with true
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
       rollsLeft: st.rollsLeft - 1
     }));
@@ -47,6 +54,7 @@ class Game extends Component {
 
   toggleLocked(idx) {
     // toggle whether idx is in locked or not
+    //using callback function with setState
     this.setState(st => ({
       locked: [
         ...st.locked.slice(0, idx),
@@ -81,6 +89,7 @@ class Game extends Component {
             <div className='Game-button-wrapper'>
               <button
                 className='Game-reroll'
+                // if all the values are true or all the values are false then disable the button
                 disabled={this.state.locked.every(x => x)}
                 onClick={this.roll}
               >
--- yahtzee-app/src/Rules.js
+++ yahtzee-app/src/Rules.js
@@ -22,8 +22,9 @@ class Rule {
   freq(dice) {
     // frequencies of dice values
     const freqs = new Map();
-    for (let d of dice) freqs.set(d, (freqs.get(d) || 0) + 1);
+    for (let d of dice) freqs.set(d, (freqs.get(d) || 0) + 1); //important: if its there increase otherwise just add 1
     return Array.from(freqs.values());
+    // Map in JavaScript. ... Map is a collection of elements where each element is stored as a Key, value pair. Map object can hold both objects and primitive values as either key or value. When we iterate over the map object it returns the key,value pair in the same order as inserted.
   }
 
   count(dice, val) {
@@ -51,8 +52,12 @@ class TotalOneNumber extends Rule {
 class SumDistro extends Rule {
   evalRoll = dice => {
     // do any of the counts meet of exceed this distro?
+    //count is passed in when instantiate
     return this.freq(dice).some(c => c >= this.count) ? this.sum(dice) : 0;
   };
+  //take freq (how many times something occur) then use the some method to know that if that array actually contain something like (4 of kind or 3 of kind ) then return true and the call the sum function on that dice
+
+  //The some() method tests whether at least one element in the array passes the test implemented by the provided function. It returns a Boolean value.
 }
 
 /** Check if full house (3-of-kind and 2-of-kind) */
@@ -72,8 +77,9 @@ class SmallStraight {
 class LargeStraight extends Rule {
   evalRoll = dice => {
     const d = new Set(dice);
-
-    // large straight must be 5 different dice & only one can be a 1 or a 6
+    //set allows only unique item
+    // large straight must be 5 different dice & only one can be a '1' or a '6'
+    //12345 or 23456
     return d.size === 5 && (!d.has(1) || !d.has(6)) ? this.score : 0;
   };
 }
@@ -83,6 +89,7 @@ class LargeStraight extends Rule {
 class Yahtzee extends Rule {
   evalRoll = dice => {
     // all dice must be the same
+    //this.freq returns array with freq and it only has 1 item if all are same
     return this.freq(dice)[0] === 5 ? this.score : 0;
   };
 }
@@ -109,7 +116,7 @@ const largeStraight = new LargeStraight({ score: 40 });
 // yahtzee scores as 50
 const yahtzee = new Yahtzee({ score: 50 });
 
-// for chance, can view as some of all dice, requiring at least 0 of a kind
+// for chance, can view as sum of all dice, requiring at least 0 of a kind
 const chance = new SumDistro({ count: 0 });
 
 export {
