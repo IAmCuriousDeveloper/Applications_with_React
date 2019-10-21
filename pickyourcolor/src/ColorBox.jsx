855dd61c5457a8c7924ee19b7baaea24d8b3dd29
Prashant rawal <Prashantrawal619@gmail.com>
2019-10-21 12:09:41 +0530

    styled colorBox

--- pickyourcolor/src/ColorBox.jsx
+++ pickyourcolor/src/ColorBox.jsx
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import "./ColorBox.css";
 export default class ColorBox extends Component {
   render() {
+    const { name, background } = this.props;
     return (
-      <div style={{ background: this.props.background }} className='ColorBox'>
-        <span>{this.props.name}</span>
-        <span>More</span>
+      <div style={{ background: background }} className='ColorBox'>
+        <div className='copy-container'>
+          <div className='box-container'>
+            <span>{name}</span>
+          </div>
+          <button className='copy-button'>Copy</button>
+        </div>
+        <span className='see-more'>More</span>
       </div>
     );
   }
