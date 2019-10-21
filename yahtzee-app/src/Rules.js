65de5204e6136fcf3bbfd80713136659af067c21
Prashant rawal <Prashantrawal619@gmail.com>
2019-10-21 09:38:41 +0530

    add comment on rules

--- yahtzee-app/src/Rules.js
+++ yahtzee-app/src/Rules.js
@@ -62,14 +62,27 @@ class SumDistro extends Rule {
 
 /** Check if full house (3-of-kind and 2-of-kind) */
 
-class FullHouse {
-  // TODO
+class FullHouse extends Rule {
+  evalRoll = dice => {
+    const frequency = this.freq(dice);
+    return frequency.includes(2) && frequency.includes(3) ? this.score : 0;
+  };
 }
 
 /** Check for small straights. */
 
-class SmallStraight {
-  // TODO
+class SmallStraight extends Rule {
+  evalRoll = dice => {
+    var d = new Set(dice);
+    //2345 1234
+    if (d.has(2) && d.has(3) && d.has(4) && (d.has(5) || d.has(1)))
+      return this.score;
+    //3456 , 2345
+    if (d.has(3) && d.has(4) && d.has(5) && (d.has(6) || d.has(2)))
+      return this.score;
+
+    return 0;
+  };
 }
 
 /** Check for large straights. */
@@ -107,10 +120,10 @@ const threeOfKind = new SumDistro({ count: 3 });
 const fourOfKind = new SumDistro({ count: 4 });
 
 // full house scores as flat 25
-const fullHouse = "TODO";
+const fullHouse = new FullHouse({ score: 25 });
 
 // small/large straights score as 30/40
-const smallStraight = "TODO";
+const smallStraight = new SmallStraight({ score: 30 });
 const largeStraight = new LargeStraight({ score: 40 });
 
 // yahtzee scores as 50
