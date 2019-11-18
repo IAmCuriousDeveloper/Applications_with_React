a4163f04fe9c3a8205ea9c11ab86a83669265ea0
Prashant rawal <Prashantrawal619@gmail.com>
2019-11-18 11:04:07 +0530

    individual palette route

--- pickyourcolor/src/App.js
+++ pickyourcolor/src/App.js
@@ -3,21 +3,35 @@ import seedColors from "./seedColors";
 import Palette from "./Palette";
 import { Route, Switch } from "react-router-dom";
 import { generatePalette } from "./colorHelpers";
-function App() {
-  // generate palette just take any palette and convert it into new palette with different shades
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>palette list</h1>} />
-      <Route
-        exact
-        path="/palette/:pid"
-        render={() => <h1>palette individual</h1>}
-      />
-    </Switch>
-    // <div>
-    //   <Palette palette={generatePalette(seedColors[6])} />
-    // </div>
-  );
+// generate palette just take any palette and convert it into new palette with different shades
+
+class App extends React.Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>palette list</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[6])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
