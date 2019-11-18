ec9151b9a4a4688ac31b03ac68364c4f2396bd34
Prashant rawal <Prashantrawal619@gmail.com>
2019-11-18 17:13:51 +0530

    start styling minipalette

--- pickyourcolor/src/PaletteList.jsx
+++ pickyourcolor/src/PaletteList.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import MiniPalette from "./Minipalette";
 
 export default class PaletteList extends Component {
   render() {
     const { palette } = this.props;
-    const paletteNames = palette.map(palette => {
-      return (
-        <p>
-          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-        </p>
-      );
+    // const allPalette = palette.map(palette => {
+    //   return (
+    //     <p>
+    //       <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+    //     </p>
+    //   );
+    // });
+    const minipalletes = palette.map(palette => {
+      return <MiniPalette {...palette} />;
     });
-    return <div>{paletteNames}</div>;
+    return (
+      <div>
+        <h1>Pick Your Colors</h1>
+        {minipalletes}
+      </div>
+    );
   }
 }
