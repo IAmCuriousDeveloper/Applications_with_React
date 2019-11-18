6521bcca755edeb2f0033b526b5deb18f59b9ea8
Prashant rawal <Prashantrawal619@gmail.com>
2019-11-18 10:42:35 +0530

    add react router

--- pickyourcolor/src/App.js
+++ pickyourcolor/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import seedColors from "./seedColors";
 import Palette from "./Palette";
+import { Route, Switch } from "react-router-dom";
 import { generatePalette } from "./colorHelpers";
 function App() {
   // generate palette just take any palette and convert it into new palette with different shades
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[6])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>palette list</h1>} />
+      <Route
+        exact
+        path="/palette/:pid"
+        render={() => <h1>palette individual</h1>}
+      />
+    </Switch>
+    // <div>
+    //   <Palette palette={generatePalette(seedColors[6])} />
+    // </div>
   );
 }
 
